6c17e8e6737b937aaf40010d7928fccdf27592af
ahmetpasa <63044445+ahmetpasa@users.noreply.github.com>
2020-04-24 18:04:49 +0300

    Printing to Excel sheet update

--- Scraping_Data.py
+++ Scraping_Data.py
@@ -1,4 +1,7 @@
 import cfscrape
+import csv
+import codecs
+import pandas as pd # You must also import "openpyxl" for running this, if you do not setup yet, it gives an error
 from bs4 import BeautifulSoup
 
 before_year = 2019
@@ -15,7 +18,25 @@ scraper_2 = cfscrape.create_scraper()
 html_source = scraper.get(british_journal_url).text
 soup = BeautifulSoup(html_source, 'lxml')
 
+csv_file = codecs.open('ex_codebook.csv', 'w', encoding='utf-8')
+
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['Journal Title', 'Coverage', 'Year', 'Volume', 'Issue', 'Title of Article', 'Variable 7']) # Column names are arranged as example
+
 journal_title = soup.find('span', 'journalTitle').text.split(',')[1].lstrip()  # Variable 1
+
+if journal_title in 'The British Journal of Sociology': # These codes are arranged as codebook
+    journal_title_code = 6 # it's not coded in codebook so we give 6
+elif journal_title in 'American Sociological Review':
+    journal_title_code = 1
+elif journal_title in 'New Perspectives on Turkey':
+    journal_title_code = 3
+elif journal_title in 'European Sociological Review':
+    journal_title_code = 5
+elif journal_title in 'Modern China':
+    journal_title_code = 4
+elif journal_title in 'Social Science Japan Journal':
+    journal_title_code = 2 
 
 if journal_title in 'American Sociological Review' or 'British Journal of Sociology':
     geographic_coverage = 2
@@ -83,8 +104,15 @@ for i in range(before_year, after_year - 1, -1):
                             print('issue number is ' + issue_number) # Variable 5
                             print('order in issue is ' + str(article_order_counter)) # Variable 7
                             print()
+                            csv_writer.writerow(
+                                [journal_title_code, geographic_coverage, year, volume_number, issue_number,
+                                 title_of_article, str(article_order_counter)])
+                            # It is printing in the console but it also gives a CSV file in the project directory
 
+csv_file.close()
 
+read_file = pd.read_csv ('ex_codebook.csv')
+read_file.to_excel ('new_codebook.xlsx', index = None, header=True) # Formatting the CSV to XLSX(Excel Table) and opens a new file in same directory.
 
 
 
