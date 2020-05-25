859d42f74ef633a3e58217c365d7e8690a4ecd07
ahmetpasa <63044445+ahmetpasa@users.noreply.github.com>
2020-05-26 01:01:19 +0300

    Added Variable 9 and few edits

--- Phase 2/Phase 2.py
+++ Phase 2/Phase 2.py
@@ -268,7 +268,139 @@ class SignUpPage:
 
 
 class MainPage:
+    
+    def Fetch_Variable_9(self, active, before_year, after_year):
+        self.variable_9 = [] #Values are recorded in this list
+        self.file = self.csv_title + '.csv'
 
+        self.update_index = 0
+
+        self.update_file = pd.read_csv(self.file)
+
+        if active is True:
+            self.database_connection = DatabaseConnector().get_conn()
+            self.cursor = self.database_connection.cursor()
+
+            self.query = 'SELECT MAX(id) FROM queries'
+            self.cursor.execute(self.query)
+            self.max_id = self.cursor.fetchone()
+
+            self.query = 'UPDATE queries SET variable_9 = %s WHERE id = %s'
+            self.values = ("variable 9", self.max_id[0])
+            self.cursor.execute(self.query, self.values)
+
+            self.database_connection.commit()
+            self.database_connection.close()
+            self.cursor.close()
+
+            for i in range(before_year, after_year - 1, -1):
+
+                self.html_source = self.scraper.get(self.british_journal_url + str(i)).text
+                self.soup = BeautifulSoup(self.html_source, 'lxml')
+
+                for self.journal in self.soup.findAll('li', class_='card clearfix'):
+
+                    self.issue_url = 'https://onlinelibrary.wiley.com' + self.journal.find('a')['href']
+                    self.issue_and_volume_info = self.journal.find('a', class_='visitable').text
+
+                    self.html_source_2 = self.scraper_2.get(self.issue_url).text
+                    self.article_soup = BeautifulSoup(self.html_source_2, 'lxml')
+
+                    for self.articles in self.article_soup.findAll('div',
+                                                                   class_='card issue-items-container exportCitationWrapper'):
+                        if not self.articles.find('h3'):
+                            for self.article_order_in_issue in self.articles.findAll('a',
+                                                                                     class_='issue-item__title visitable'):
+                                if any(self.word in self.article_order_in_issue.h2.text for self.word in
+                                       self.articles_we_wont_scrape):
+                                    pass
+                                else:
+                                    try:
+                                        self.abstract_scrape = self.article_order_in_issue.find_next_sibling('div',
+                                                                                                             class_='content-item-format-links')
+
+                                        self.abstract_scrape_2 = self.abstract_scrape.find('a', title='Abstract')[
+                                            'href']
+
+                                        self.title_of_article = self.article_order_in_issue.h2.text
+
+                                        #All titles are coded in here
+                                        #If we see an interval, we can say easily that includes year
+                                        #but also for extreme cases(ex. 3-5) it's coded as 4 digit nums
+
+                                        match = re.search(r'.*([1-3][0-9]{3})(-|–).*([1-3][0-9]{3})',
+                                                          self.title_of_article)
+
+                                        #if there's no interval, we search the year, coding philosophy is the same
+
+                                        match2 = re.search(r'.*([1-3][0-9]{3})', self.title_of_article)
+
+                                        #Then we search "century" phrase but for all cases we search "th century"
+                                        #Uppercase cases are also included
+
+                                        match3 = re.search(
+                                            r'(rd|st|nd|th) \bCentur[a-z]*\b|(rd|st|nd|th) \bcentur[a-z]*\b',
+                                            self.title_of_article)
+
+                                        #If there is a year or century phrase, this is enough for code as '1'
+
+                                        if match is not None:
+                                            self.variable_9.append('1')
+                                        elif match2 is not None:
+                                            self.variable_9.append('1')
+                                        elif match3 is not None:
+                                            self.variable_9.append('1')
+                                        else:
+                                            self.variable_9.append('0')
+                                    except:
+                                        pass
+                        else:
+                            if any(self.word in self.articles.h3.text for self.word in self.articles_we_wont_scrape):
+                                pass
+                            else:
+                                for self.article_order_in_issue in self.articles.findAll('a',
+                                                                                         class_='issue-item__title visitable'):
+                                    if any(self.word in self.article_order_in_issue.h2.text for self.word in
+                                           self.articles_we_wont_scrape):
+                                        pass
+                                    else:
+                                        try:
+                                            self.abstract_scrape = self.article_order_in_issue.find_next_sibling('div',
+                                                                                                                 class_='content-item-format-links')
+
+                                            self.abstract_scrape_2 = self.abstract_scrape.find('a', title='Abstract')[
+                                                'href']
+
+                                            self.title_of_article = self.article_order_in_issue.h2.text
+
+                                            #Same approach as upper lines
+
+                                            match = re.search(r'.*([1-3][0-9]{3})(-|–).*([1-3][0-9]{3})',
+                                                              self.title_of_article)
+                                            match2 = re.search(r'.*([1-3][0-9]{3})', self.title_of_article)
+                                            match3 = re.search(
+                                                r'(rd|st|nd|th) \bCentur[a-z]*\b|(rd|st|nd|th) \bcentur[a-z]*\b',
+                                                self.title_of_article)
+                                            if match is not None:
+                                                self.variable_9.append('1')
+                                            elif match2 is not None:
+                                                self.variable_9.append('1')
+                                            elif match3 is not None:
+                                                self.variable_9.append('1')
+                                            else:
+                                                self.variable_9.append('0')
+                                        except:
+                                            pass
+
+            # Inserting the values into the file
+            self.update_file['Variable 9'] = self.variable_9
+
+            self.update_file.to_csv(self.file, index=False)
+
+            self.update_file.to_excel(self.csv_title + '_excel_format_' + '.xlsx', index=None, header=True)
+        else:
+            pass 
+    
     def isSpecialIssue(self, row):
         # Defining if a row is part of a special issue
         try:
@@ -286,36 +418,41 @@ class MainPage:
 
         # Fetching Variable 13
 
-        self.database_connection = DatabaseConnector().get_conn()
-        self.cursor = self.database_connection.cursor()
+        if active is True:
 
-        self.query = 'SELECT MAX(id) FROM queries'
-        self.cursor.execute(self.query)
-        self.max_id = self.cursor.fetchone()
+            self.database_connection = DatabaseConnector().get_conn()
+            self.cursor = self.database_connection.cursor()
 
-        self.query = 'UPDATE queries SET variable_13 = %s WHERE id = %s'
-        self.values = ("variable 13", self.max_id[0])
-        self.cursor.execute(self.query, self.values)
+            self.query = 'SELECT MAX(id) FROM queries'
+            self.cursor.execute(self.query)
+            self.max_id = self.cursor.fetchone()
 
-        self.database_connection.commit()
+            self.query = 'UPDATE queries SET variable_13 = %s WHERE id = %s'
+            self.values = ("variable 13", self.max_id[0])
+            self.cursor.execute(self.query, self.values)
 
-        self.database_connection.close()
-        self.cursor.close()
+            self.database_connection.commit()
 
-        self.file = self.csv_title + '.csv'
+            self.database_connection.close()
+            self.cursor.close()
 
-        self.update_index = 0
+            self.file = self.csv_title + '.csv'
 
-        # reading the file to be edited
-        self.update_file = pd.read_csv(self.file)
+            self.update_index = 0
 
-        # writing the results in a new row in the csv and excel files
+            # reading the file to be edited
+            self.update_file = pd.read_csv(self.file)
 
-        self.update_file['Variable 13'] = self.update_file.apply(self.isSpecialIssue, axis=1)
+            # writing the results in a new row in the csv and excel files
 
-        self.update_file.to_csv(self.file, index=False)
+            self.update_file['Variable 13'] = self.update_file.apply(self.isSpecialIssue, axis=1)
 
-        self.update_file.to_excel(self.csv_title + '_excel_format_' + '.xlsx', index=None, header=True)
+            self.update_file.to_csv(self.file, index=False)
+
+            self.update_file.to_excel(self.csv_title + '_excel_format_' + '.xlsx', index=None, header=True)
+
+        else:
+            pass
 
 
 
@@ -635,6 +772,7 @@ class MainPage:
         self.read_file.to_excel(self.csv_title + '_excel_format_' + '.xlsx', index=None,header=True)  # Formatting the CSV to XLSX(Excel Table)
 
 
+        self.Fetch_Variable_9(True, self.before_year, self.after_year)
         self.Fetch_Variable_10(True, self.before_year, self.after_year)
         self.Fetch_Variable_13(True)
         self.statusbar.destroy()
